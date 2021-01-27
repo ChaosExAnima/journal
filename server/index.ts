c4ef0ad4a7a7f1d5ee4716c03f5710783b8b8e16
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2021-01-27 16:20:20 -0500

    Adds logic to serve journal entries.

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,18 @@
 import fastify from 'fastify';
 import { resolve } from 'path';
+import { promises as fs } from 'fs';
+import { mdToDraftjs } from 'draftjs-md-converter';
+
 import { version } from '../package.json';
 
 const server = fastify( { logger: true } );
 
+const entriesDir = resolve( __dirname, '..', 'entries' );
+const staticDir = resolve( __dirname, '..', 'build' );
+
 // Plugins.
 server.register( import( 'fastify-static' ), {
-	root: resolve( __dirname, '..', 'build' ),
+	root: staticDir,
 	wildcard: false,
 } );
 server.register( import( 'fastify-helmet' ), {
@@ -23,10 +29,47 @@ server.get( '/api', async () => ( {
 } ) );
 
 // Serves journal entries.
-server.get( '/api/entries', async () => [] );
+server.get( '/api/entries', async ( req, reply ) => {
+	const entries = ( await fs.readdir( entriesDir ) )
+		.filter( ( file ) => file.endsWith( '.md' ) )
+		.map( ( file ) => file.replace( '.md', '' ) );
+	reply.send( entries );
+} );
+
+type GetEntry = {
+	Querystring: {
+		date?: string;
+	};
+};
+
+// Serve entry in format that Draft.js can understand.
+server.get< GetEntry >( '/api/entry', async ( req, reply ) => {
+	const { date } = req.query;
+	if ( ! date ) {
+		return reply
+			.code( 400 )
+			.send( new Error( 'Please provide an entry date' ) );
+	}
+
+	if ( ! /^\d{4}-\d{2}-\d{2}$/.test( date ) ) {
+		return reply.code( 400 ).send( new Error( 'Invalid entry date' ) );
+	}
+
+	try {
+		const rawEntry = await fs.readFile(
+			resolve( entriesDir, `${ date }.md` ),
+			'utf-8'
+		);
+		const entry = mdToDraftjs( rawEntry );
+		return reply.send( entry );
+	} catch ( err ) {
+		server.log.error( err );
+		return reply.code( 404 ).send( new Error( 'Entry not found' ) );
+	}
+} );
 
 // Saves a journal entry.
-server.post( '/api/save', async () => ( { success: true } ) );
+server.post( '/api/entry', async () => ( { success: true } ) );
 
 // Serves the built file for all other paths.
 server.get( '*', async ( req, reply ) => {
